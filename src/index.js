e7738247aaff7f6df7797745b3219cf44345bb1d
janethpc <108207854+janethpc@users.noreply.github.com>
2022-07-25 10:43:57 -0500

    AVANCES

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import validator from './validator.js';
+//import validator from './validator.js';//
+import {escribirMensajeConsola} from "./validator.js"; 
+//console.log(validator);//
 
-console.log(validator);
+var datosinf="¡Valida tus datos bancarios! ";
+
+
+document.getElementById("prueba").addEventListener("click", escribirMensajeConsola, false);
+
+
+
+window.onload= function(){
+    document.getElementById("buttonValidarCampos").innerHTML= datosinf;
+}
+
+//funcion para al presionar el button se ejecute la validacion// 
+function  CodigoValidacion(cardnumber) {
+
+    var validarTarjeta=cardnumber.value;
+    console.log(validarTarjeta);
+   
+    var numberReverse=validarTarjeta.split("").reverse().toString();
+    console.log(numberReverse);
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,17 @@
-const validator = {
-  // ...
-};
+//const validator = {
+  //(property) maskify: (valor: any) => Any
+  //maskify: valor=> valor.slice(0, -4).replace(/./g, "#") + valor.slice(-4),
 
-export default validator;
+  //funion para comenzar a validar el cc number//
+  //isValid: function isValid(){
+    //let valid = false
+    //let number= [] 
+  //}
+//};
+
+//export default validator;
+
+export function escribirMensajeConsola(){
+  var funcionaBien = document.getElementById("textoPrueba").value; 
+  console.log(funcionaBien);
+}
